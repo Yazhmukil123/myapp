93e7aa685480e21acabbc57ee383ac1a5409d95f
Your Name <you@example.com>
2022-08-19 09:31:36 +0530

    Updates

---
+++ precache-manifest.95c6e902c7985c8d0c2248aa6da3ee34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3260941cfa9871e9e8d",
+    "url": "/react_upload/static/css/main.e4fa57ff.chunk.css"
+  },
+  {
+    "revision": "a3260941cfa9871e9e8d",
+    "url": "/react_upload/static/js/main.a3260941.chunk.js"
+  },
+  {
+    "revision": "e1f1d05d65ad0a0ddf05",
+    "url": "/react_upload/static/js/1.e1f1d05d.chunk.js"
+  },
+  {
+    "revision": "1f7efc12cdeba9e3d821",
+    "url": "/react_upload/static/js/runtime~main.1f7efc12.js"
+  },
+  {
+    "revision": "82b90a5b2bfde177faa1d550ca98f523",
+    "url": "/react_upload/index.html"
+  }
+];
